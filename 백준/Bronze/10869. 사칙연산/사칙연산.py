160b94395473b54d2b3db39d3cbedb98e3705e9b
ehddnjsKim <kdo316@naver.com>
2025-07-29 09:14:39 +0900

    [Bronze V] Title: 사칙연산, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Bronze/10869. 사칙연산/사칙연산.py
+++ 백준/Bronze/10869. 사칙연산/사칙연산.py
@@ -1,13 +1,9 @@
-# 두 자연수를 입력받아 공백 기준으로 분리
-a, b = input().split()
+def calculate(a, b):
+    print(a + b)   # 덧셈
+    print(a - b)   # 뺄셈
+    print(a * b)   # 곱셈
+    print(a // b)  # 정수 나눗셈 (몫)
+    print(a % b)   # 나머지
 
-# 문자열을 정수로 변환
-a = int(a)
-b = int(b)
-
-# 각 연산 결과 출력
-print(a + b)   # 덧셈
-print(a - b)   # 뺄셈
-print(a * b)   # 곱셈
-print(a // b)  # 나눗셈 (몫)
-print(a % b)   # 나머지
+a, b = map(int, input().split())
+calculate(a, b)
